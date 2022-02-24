7fe0e7ae14c09f704ac0f63a11d6aaaf0a98b344
Soheab Khan <skhangns@gmail.com>
2022-02-23 20:39:12 -0500

    connected connection file to index.js

--- index.js
+++ index.js
@@ -1,2 +1,47 @@
-const mysql = require('mysql2')
 const inquirer = require('inquirer');
+const connection = require('./connection/connection');
+
+
+
+connection.connect(()=>{
+    promptstart()
+})
+
+
+
+function promptstart() {
+    inquirer.prompt([
+      {
+        name: "choice",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+          "View all departments",
+          "View all roles",
+          "View all employees",
+          "Add a new department",
+          "Add a new role",
+          "Add a new employee",
+          "Update employee roles",
+          "Exit"
+        ]
+      }])
+      .then(function (res) {
+        switch (res.choice) {
+          case "View all departments":
+            viewDepartments();
+            break;
+          case "View all roles":
+            viewRoles();
+            break;
+            case "View all employees":
+                viewAllEmployees();
+                break;
+            case "Exit":
+                connection.end();
+                break;
+
+        }})
+
+            }
+
